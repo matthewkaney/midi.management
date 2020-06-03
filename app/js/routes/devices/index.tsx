681eb8e7774bd98f3cfee2791d6d79da1bfe4319
Matthew Kaney <matthew@mindofmatthew.com>
2020-06-03 11:38:55 -0400

    Check in rough version of devices page

--- app/js/routes/devices/index.tsx
+++ app/js/routes/devices/index.tsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { receiveMidiInputs, receiveMidiOutputs } from '@musedlab/midi/web';
 
 import { Header } from '../../components/header/Header';
 
+import './style.css';
+
 export function MidiDevices() {
+  const [inputs, setInputs] = useState<MIDIInput[]>([]);
+  useEffect(() =>
+    receiveMidiInputs((i) => {
+      setInputs(i);
+    })
+  );
+
+  const [outputs, setOutputs] = useState<MIDIOutput[]>([]);
+  useEffect(() =>
+    receiveMidiOutputs((o) => {
+      setOutputs(o);
+    })
+  );
+
   return (
     <>
       <Header />
-      <main></main>
+      <main>
+        <div className="container">
+          <div className="device-list">
+            <h2>Inputs</h2>
+            <ul>
+              {inputs.map((i) => (
+                <li key={i.id}>
+                  {i.name}
+                  {i.manufacturer ? `: ${i.manufacturer}` : null}
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="device-list">
+            <h2>Outputs</h2>
+            <ul>
+              {outputs.map((i) => (
+                <li key={i.id}>
+                  {i.name}
+                  {i.manufacturer ? `: ${i.manufacturer}` : null}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </main>
     </>
   );
 }
