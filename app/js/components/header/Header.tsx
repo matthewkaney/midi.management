efcf826f5ea4434b8078c0d58fa8af31fa3e3bb0
Matthew Kaney <matthew@mindofmatthew.com>
2020-07-06 08:30:13 -0400

    Improve styling of form elements

--- app/js/components/header/Header.tsx
+++ app/js/components/header/Header.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { useLocation, Link } from '@reach/router';
+import React from "react";
+import { useLocation, Link } from "@reach/router";
 
 import {
   Header as BaseHeader,
   Menu,
   MenuItem,
-} from '../../../../management-ui/components/Header';
+} from "../../../../management-ui/components/Header";
 
-const links = ['devices', 'monitor'];
+const links = ["devices", "monitor"];
 
 type HeaderProps = {
   children?: React.ReactNode;
@@ -15,19 +15,17 @@ type HeaderProps = {
 
 export function Header({ children }: HeaderProps) {
   const { pathname } = useLocation();
-  const app = pathname.split('/')[1];
+  const app = pathname.split("/")[1];
 
   return (
     <BaseHeader
-      title={
-        <Menu current={<h1>{app}</h1>}>
-          {links.map((l) => (
-            <MenuItem key={l}>
-              <Link to={`/${l}`}>{l}</Link>
-            </MenuItem>
-          ))}
-        </Menu>
-      }>
+      title={app}
+      menu={links.map((l) => (
+        <MenuItem key={l}>
+          <Link to={`/${l}`}>{l}</Link>
+        </MenuItem>
+      ))}
+    >
       {children}
     </BaseHeader>
   );
