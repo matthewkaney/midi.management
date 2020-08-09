bf4f1276cae29f8f92c0a9ee635c650a141cd68d
Matthew <matthew@mindofmatthew.com>
2020-08-09 15:44:25 -0400

    Add landing screen and disable routes for now

--- app/js/main.tsx
+++ app/js/main.tsx
@@ -1,28 +1,30 @@
-import React from 'react';
-import { render } from 'react-dom';
-import { Router } from '@reach/router';
+import React from "react";
+import { render } from "react-dom";
+import { Router } from "@reach/router";
 
-import { MidiDevices } from './routes/devices';
-import { MidiMonitor } from './routes/monitor';
-import { MidiViewer } from './routes/viewer';
-import { MidiLive } from './routes/live';
+import { LandingPage } from "./routes/landing";
+import { MidiDevices } from "./routes/devices";
+import { MidiMonitor } from "./routes/monitor";
+import { MidiViewer } from "./routes/viewer";
+import { MidiLive } from "./routes/live";
 
 // Dev Only
-import { StyleGuide } from '../../management-ui/styleguide';
+import { StyleGuide } from "../../management-ui/styleguide";
 
 function MidiManagement() {
   return (
     <Router>
-      <MidiDevices path="devices" />
-      <MidiViewer path="viewer" />
-      <MidiMonitor path="monitor" />
-      <MidiLive path="live/*" />
+      {/* <MidiDevices path="devices" /> */}
+      {/* <MidiViewer path="viewer" /> */}
+      {/* <MidiMonitor path="monitor" /> */}
+      {/* <MidiLive path="live/*" /> */}
+      <LandingPage path="/" />
 
-      {process.env.NODE_ENV === 'development' ? <StyleGuide path="ui" /> : null}
+      {process.env.NODE_ENV === "development" ? <StyleGuide path="ui" /> : null}
     </Router>
   );
 }
 
 window.onload = () => {
-  render(<MidiManagement />, document.getElementById('root'));
+  render(<MidiManagement />, document.getElementById("root"));
 };
---
+++ app/js/routes/landing/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import "./style.css";
+
+import { RouteComponentProps } from "@reach/router";
+
+export function LandingPage(props: RouteComponentProps) {
+  return (
+    <main>
+      <div className="landing-header">
+        <h1 className="big">midi.management</h1>
+        <p>
+          Useful tools for working with MIDI sequences, messages and hardware in
+          the browser.
+        </p>
+        <p>Coming soon...</p>
+      </div>
+    </main>
+  );
+}
